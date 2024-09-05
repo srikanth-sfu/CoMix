7d7e74ee50e711b009432c7ec21adc7be7f17e65
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-09-05 13:16:13 -0400

    simplified debug

--- utils.py
+++ utils.py
@@ -101,7 +101,7 @@ def select_folders_zip(src_zip_filename: str, tgt_filename: str, paths: Set):
     select_folders_to_zip(src,tgt_filename)
 
 if __name__ == "__main__":
-    root = "%s/%s"%(os.getenv('SLURM_TMPDIR'), fn)
+    root = "%s"%(os.getenv('SLURM_TMPDIR'))
     src_list = zipfile.ZipFile(f"{root}/ucf_BG.zip", 'r').namelist()
     src_list = set([os.path.basename(x) for x in src_list if x[-1] == "/" and x != "ucf_BG/"])
     tgt_zip = f"{root}/ucf_videos.zip"
