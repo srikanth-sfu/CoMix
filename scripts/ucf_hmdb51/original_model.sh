fa3f7fa3adbdff3fe774da0496e96302ce88534b
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-09-09 18:17:24 -0400

    add timeout loop

--- scripts/ucf_hmdb51/original_model.sh
+++ scripts/ucf_hmdb51/original_model.sh
@@ -3,34 +3,31 @@
 # SLURM script for job resubmission on our clusters. 
 # ---------------------------------------------------------------------
 #SBATCH --job-name=baseline_run
-#SBATCH --account=def-mpederso
+#SBATCH --account=rrg-mpederso
 #SBATCH --mem-per-cpu=64G
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
-#SBATCH --time=0-00:30
+#SBATCH --time=0-01:00
 #SBATCH -o /home/smuralid/error/tubelets/comix/slurm-%j.out  # Write the log on scratch
 #SBATCH -e /home/smuralid/error/tubelets/comix/slurm-%j.err
 source activate focal
-cp /scratch/smuralid/datasets/ucf_hmdb_condensed.zip $SLURM_TMPDIR
 cd $SLURM_TMPDIR
+mkdir ucf_hmdb
+cp /scratch/smuralid/datasets/ucf_hmdb_condensed.zip $SLURM_TMPDIR/ucf_hmdb
+cd ucf_hmdb
 unzip ucf_hmdb_condensed.zip
+cd $SLURM_TMPDIR
 
 git clone git@github.com:srikanth-sfu/CoMix.git
 cd CoMix
 git checkout baseline_splitpretrain
-python main.py --manual_seed 1 --dataset_name UCF-HMDB \
---src_dataset UCF --tgt_dataset HMDB --batch_size 4 --model_root /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline/ --save_in_steps 500 \
---log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True --num_iter_warmstart 4000 \
---num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.1 --lambda_tpl 0.01 \
---base_dir $SLURM_TMPDIR/ucf_hmdb/ --warmstart_graph_checkpoint /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline_ws/
-
-
-# Resubmit if not all work has been done yet.
-# You must define the function work_should_continue().
-if work_should_continue; then
-     sbatch ${BASH_SOURCE[0]}
+timeout 179m python main.py --manual_seed 1 --dataset_name UCF-HMDB --src_dataset UCF --tgt_dataset HMDB --batch_size 8 --model_root /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline/ --save_in_steps 500 --num_segments 0 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True --num_iter_warmstart 4000 --num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.1 --lambda_tpl 0.01 --base_dir $SLURM_TMPDIR/ucf_hmdb/ --warmstart_graph_checkpoint /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline_ws/
+if [ $? -eq 124 ]; then
+  echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
+  # Call the script itself again with the same configuration
+  sbatch scripts/ucf_hmdb51_original_model.sh
+  # scontrol requeue $SLURM_JOB_ID
+else
+  echo "The script finished before timing out."
+  # Exit or perform any other necessary cleanup
 fi
-
-# ---------------------------------------------------------------------
-echo "Job finished with exit code $? at: `date`"
-# ---------------------------------------------------------------------
