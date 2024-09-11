83f09df4e7624e5889d880696395683696b44f75
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-09-11 09:04:00 -0400

    test accuracy

--- core/adapt.py
+++ core/adapt.py
@@ -485,7 +485,7 @@ def warmstart_models(graph_model, i3d_online, src_data_loader, tgt_data_loader=N
     if not os.path.exists(params.model_root):
         os.makedirs(params.model_root)
 
-
+    print(start_iter,num_iterations)
     for itrn in range(start_iter, num_iterations):
         print("\rRunning Iteration (source-only) : {}/{}".format(itrn, num_iterations), end='', flush=True)
         if itrn%100 == 0:
