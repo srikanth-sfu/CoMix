a577e2a82ada5ebfad8f5502cc77390f67a5167c
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-09-11 16:56:24 -0400

    add auto resume

--- scripts/ucf_hmdb51/adapt_scripts/original_model.sh
+++ scripts/ucf_hmdb51/adapt_scripts/original_model.sh
@@ -23,7 +23,7 @@ git clone git@github.com:srikanth-sfu/CoMix.git
 cd CoMix
 git checkout baseline_splitpretrain
 echo "------------------------------"
-timeout 175m python main.py --manual_seed 1 --dataset_name UCF-HMDB --src_dataset UCF --tgt_dataset HMDB --batch_size 7 --model_root /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline/ --save_in_steps 500 --num_segments 0 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True --num_iter_warmstart 4000 --num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.1 --lambda_tpl 0.01 --base_dir $SLURM_TMPDIR/ucf_hmdb/ --warmstart_graph /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline_ws/Graph-SourceOnly-Model-Best.pth --warmstart_i3d /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline_ws/I3D-SourceOnly-Online-Model-Best.pth
+timeout 175m python main.py --manual_seed 1 --auto_resume True --dataset_name UCF-HMDB --src_dataset UCF --tgt_dataset HMDB --batch_size 7 --model_root /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline/ --save_in_steps 500 --num_segments 0 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True --num_iter_warmstart 4000 --num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.1 --lambda_tpl 0.01 --base_dir $SLURM_TMPDIR/ucf_hmdb/ --warmstart_graph /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline_ws/Graph-SourceOnly-Model-Best.pth --warmstart_i3d /scratch/smuralid/checkpoints/da/ucf_hmdb/original_baseline_ws/I3D-SourceOnly-Online-Model-Best.pth
 if [ $? -eq 124 ]; then
   echo "The script timed out after ${MAX_HOURS} hour(s). Restarting..."
   # Call the script itself again with the same configuration
