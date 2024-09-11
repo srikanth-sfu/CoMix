060206390ff273d85aaae45b4e86fb7eea02e49d
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-09-11 07:29:05 -0400

    orig script

--- scripts/ucf_hmdb51/original_model.sh
+++ scripts/ucf_hmdb51/original_model.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=rrg-mpederso
 #SBATCH --mem-per-cpu=64G
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:p100l:4
+#SBATCH --gres=gpu:4
 #SBATCH --time=0-03:00
 #SBATCH -o /home/smuralid/error/tubelets/comix/slurm-%j.out  # Write the log on scratch
 #SBATCH -e /home/smuralid/error/tubelets/comix/slurm-%j.err
