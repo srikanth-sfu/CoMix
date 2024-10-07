4d9e71e95f0c874e787246951fca282ac1db73fc
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-10-06 20:15:14 -0400

    git checkout bug

--- scripts/ucf_hmdb51/adapt_scripts/ncl_baseline_scales_32_48.sh
+++ scripts/ucf_hmdb51/adapt_scripts/ncl_baseline_scales_32_48.sh
@@ -22,7 +22,7 @@ cd $SLURM_TMPDIR
 
 git clone git@github.com:srikanth-sfu/CoMix.git
 cd CoMix
-git checkout tubelet_contrast_comix
+git checkout tubelet_contrast_comix_no_transformation
 echo "------------------------------"
 timeout 170m python main.py --manual_seed 1 --tubelet_config cfg/tubelets/scales_32_48.json --auto_resume True --dataset_name UCF-HMDB --src_dataset UCF --tgt_dataset HMDB --batch_size 7 --model_root /scratch/smuralid/checkpoints/da/ucf_hmdb/ncl_comix_baseline_video_ncl_notransform/ --save_in_steps 500 --num_segments 0 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True --num_iter_warmstart 4000 --num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.1 --lambda_tpl 0.01 --base_dir $SLURM_TMPDIR/ucf_hmdb/ --warmstart_graph /scratch/smuralid/checkpoints/da/ucf_hmdb/ncl_baseline_ws/Graph-SourceOnly-Model-Best.pth --warmstart_i3d /scratch/smuralid/checkpoints/da/ucf_hmdb/ncl_baseline_ws/I3D-SourceOnly-Online-Model-Best.pth
 if [ $? -eq 124 ]; then
