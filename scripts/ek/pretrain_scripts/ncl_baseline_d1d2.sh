f3673c46ace919ca10c5c70252eb0179822c3902
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-11-21 16:59:11 -0500

    add lmdb loader

--- scripts/ek/pretrain_scripts/ncl_baseline_d1d2.sh
+++ scripts/ek/pretrain_scripts/ncl_baseline_d1d2.sh
@@ -18,24 +18,24 @@ module load StdEnv/2020 python/3.8.10
 source focal/bin/activate
 mkdir epic_kitchens
 # D1: P08, D2: P01
-cp /project/def-mpederso/smuralid/datasets/epic_kitchens/epic_kitchens_d1.zip $SLURM_TMPDIR/epic_kitchens
-cp /project/def-mpederso/smuralid/datasets/epic_kitchens/epic_kitchens_d2.zip $SLURM_TMPDIR/epic_kitchens
+cp /project/def-mpederso/smuralid/datasets/epic_kitchens/epic_kitchens_d1_lmdb.zip $SLURM_TMPDIR/epic_kitchens
+cp /project/def-mpederso/smuralid/datasets/epic_kitchens/epic_kitchens_d2_lmdb.zip $SLURM_TMPDIR/epic_kitchens
 cp /project/def-mpederso/smuralid/datasets/epic_kitchens/epic_kitchens_bg.zip $SLURM_TMPDIR/epic_kitchens
 
 cd epic_kitchens
-unzip -qq epic_kitchens_d1.zip
-unzip -qq epic_kitchens_d2.zip
+unzip -qq epic_kitchens_d1_lmdb.zip
+unzip -qq epic_kitchens_d2_lmdb.zip
 unzip -qq epic_kitchens_bg.zip
 cd $SLURM_TMPDIR
 
 git clone git@github.com:srikanth-sfu/CoMix.git
 cd CoMix
 git checkout tubelet_contrast_comix_pretrain_adapt_moco
-bash scripts/preprocessing/unzip_ek.sh
+# bash scripts/preprocessing/unzip_ek.sh
 
 echo "------------------------------"
 
-CUDA_VISIBLE_DEVICES=0 timeout 163m python main.py --manual_seed 1 --auto_resume True  --dataset_name Epic-Kitchens \
+CUDA_VISIBLE_DEVICES=0 timeout 150m python main.py --manual_seed 1 --auto_resume True  --dataset_name Epic-Kitchens \
  --src_dataset D1 --tgt_dataset D2 --batch_size 32 --model_root /project/def-mpederso/smuralid/checkpoints/da/ek/d1d2_ncl_comix_baseline_video_pretrain_adapt_moen/ \
  --save_in_steps 500 --num_segments 0 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True \
  --num_iter_warmstart 1500 --num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.1 --lambda_tpl 0.01 \
