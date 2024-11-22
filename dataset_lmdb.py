7efa55da0882cef1e00374d4b726a7e04e45d719
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-11-21 22:17:53 -0500

    dbg

--- dataset_lmdb.py
+++ dataset_lmdb.py
@@ -151,7 +151,7 @@ class VideoDataset_EpicKitchens(Dataset):
         label = self.verb_class[idx] 
         rgb_files = self.file_lookup[path.replace(os.getenv("SLURM_TMPDIR"), "").replace("epic_kitchens", "")]
         rgb_files.sort()
-        print(len(rgb_files))
+        print(len(rgb_files), type(self.file_lookup))
         rgb_files = rgb_files[self.start_frame[idx]:self.stop_frame[idx]]
         frame_indices = np.arange(len(rgb_files))
         num_frames = len(rgb_files)
