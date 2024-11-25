b03b67b426fc452989db6b607a10573e7d65f098
srikanth-sfu <srikanth.srikanth18@gmail.com>
2024-11-25 10:46:31 -0500

    updated scripts

--- scripts/epic_kitchens_d1d2/adapt_scripts/original_model.sh
+++ scripts/epic_kitchens_d1d2/adapt_scripts/original_model.sh
@@ -31,9 +31,9 @@ git checkout jester_baseline_zip
 echo "------------------------------"
 cp /project/def-mpederso/smuralid/datasets/epic_kitchens/d1d2.pkl .
 
-timeout 150m python main.py --manual_seed 1 --dataset_name Epic-Kitchens --src_dataset D1 --tgt_dataset D2 \ 
---batch_size 8 --model_root /project/def-mpederso/smuralid/checkpoints/da/epic_kitchens_d1d2/original_baseline/ \
---save_in_steps 500 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True \
+timeout 150m python main.py --manual_seed 1 --dataset_name Epic-Kitchens --src_dataset D1 --tgt_dataset D2 \
+ --batch_size 8 --model_root /project/def-mpederso/smuralid/checkpoints/da/epic_kitchens_d1d2/original_baseline/ \
+ --save_in_steps 500 --log_in_steps 50 --eval_in_steps 50 --pseudo_threshold 0.7 --warmstart_models True \
  --num_iter_warmstart 4000 --num_iter_adapt 10000 --learning_rate 0.01 --learning_rate_ws 0.01 --lambda_bgm 0.01 \
  --lambda_tpl 0.01 --base_dir $SLURM_TMPDIR/epic_kitchens/ \
  --warmstart_graph /project/def-mpederso/smuralid/checkpoints/da/epic_kitchens_d1d2/original_baseline_ws/Graph-SourceOnly-Model-Best.pth \
